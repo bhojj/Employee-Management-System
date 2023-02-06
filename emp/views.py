51866346938f45c095d4b393109c5b4b74845ee0
bhojj <bhoj.181308@ncit.edu.np>
2023-02-06 15:16:48 +0545

    Working with database

--- emp/views.py
+++ emp/views.py
@@ -1,10 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Emp
 
 # Create your views here.
 def emp_home(request):
+    emps=Emp.objects.all()
     # return HttpResponse("Student Home Page")
-    return render(request,"emp/home.html",{})
+    return render(request,"emp/home.html",{'emps':emps})
 
 def add_emp(request):
+    if request.method=="POST":
+        
+
+        # DATA FETCH GARNE YETA BATA 
+        emp_name=request.POST.get("emp_name")
+        emp_id=request.POST.get("emp_id")
+        emp_phone=request.POST.get("emp_phone")
+        emp_address=request.POST.get("emp_address")
+        emp_working=request.POST.get("emp_working")
+        emp_department=request.POST.get("emp_department")
+        # create the model objects and set the data
+
+        e=Emp()
+        e.name=emp_name
+        e.emp_id=emp_id
+        e.phone=emp_phone
+        e.address=emp_address
+        e.department=emp_department
+        if emp_working is None:
+            e.working = False
+        else:
+            e.working = True
+        # save the object
+        e.save()
+        # prepare the message
+        # print ("Data is coming")
+        return redirect("/emp/home")
     return render(request,'emp/add_emp.html',{})
